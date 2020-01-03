afe83785613d7ea389c1da3b86134c2e651c11b7
Micco92379 <1994707977@qq.com>
2020-01-03 15:52:50 +0800

    add to develop

--- test.c
+++ test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+int add(int a,int b)
+{
+	return a+b
+}
+
 int main(int argc, const char *argv[])
 {
-	print("hello world")
+	int sum  = 0;
+	print("hello world");
+
+	sum = add(5,8);
+
+	print("sum=%d\n",sum);
+	
 	return 0;
 }
